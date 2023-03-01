56b7727b6884ce42fe58bc7e660f02682c0f5fa4
Precious Olusola <preciousolusola16@gmail.com>
2023-03-01 10:43:17 +0100

    updated tabs logic

--- tabs.js
+++ tabs.js
@@ -16,23 +16,22 @@ function changeTabFocus(e){
     // Change the tabindex of current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute('tabindex', -1);
-    }
+        
+        // if the right key is pushed, move to the next tab on the right
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length)  tabFocus = 0;
+        }
+        
+        // If the left key is pushed, move to the next tab on the left
+        else {
+            tabFocus--;
+            if (tabFocus < 0)  tabFocus = tabs.length - 1;
+        }
 
-
-    // if the right key is pushed, move to the next tab on the right
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length)  tabFocus = 0;
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-    
-    // If the left key is pushed, move to the next tab on the left
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0)  tabFocus = tabs.length - 1;
-    }
-
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e){
