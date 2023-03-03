e892fafcb1c103676d0def018a3d72190eedafa8
Precious Olusola <preciousolusola16@gmail.com>
2023-03-03 09:29:08 +0100

    Updated sidebar logic to auto-close

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ const nav =  document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 //  When the hamburger is clicked
-navToggle.addEventListener("click", ()=>{
+navToggle.addEventListener("click", toggleSidebar);
+
+document.getElementById("main").addEventListener("click", ()=>toggleSidebar(false));
+
+
+function toggleSidebar(state=null){
     const visibility = nav.getAttribute("data-visible");
-    
+
+    // If state is defined, set bar visibilty to state
+    if (state !== null){
+        nav.setAttribute('data-visible', Boolean(state));
+        navToggle.setAttribute("aria-expanded", Boolean(state));
+    }
     // If the menu is closed, open it
-    if (visibility === 'false') {
+    else if (visibility === 'false') {
         nav.setAttribute('data-visible', true);
         navToggle.setAttribute("aria-expanded", true);
     }
@@ -15,4 +25,4 @@ navToggle.addEventListener("click", ()=>{
         nav.setAttribute('data-visible', false);
         navToggle.setAttribute("aria-expanded", false);
     }
-})
+}
